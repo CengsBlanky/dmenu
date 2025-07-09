d43ef6ea62dadf4056e5b6a65a5ab16ce9cfff5b
zengshuai <zengs1994@gmail.com>
2025-07-09 20:02:42 +0800

    center dmenu

--- config.def.h
+++ config.def.h
@@ -2,9 +2,12 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 600;                    /* minimum width when centered */
+static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"CourierPrime:size=15"
+	"CourierPrime:size=18"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -14,7 +17,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines = 6;
+static unsigned int lines = 8;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -23,4 +26,4 @@ static unsigned int lines = 6;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 4;
+static unsigned int border_width = 6;
