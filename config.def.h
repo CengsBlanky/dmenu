e6e6d9cdd623f67abc739fc234060eda6f4136c1
zengshuai <zengs1994@gmail.com>
2025-07-09 23:23:17 +0800

    update font config

--- config.def.h
+++ config.def.h
@@ -7,8 +7,9 @@ static int min_width = 600;                    /* minimum width when centered */
 static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"CourierPrime:size=18",
-    "GoMono Nerd Font:size=16"
+	"Courier Prime:size=18",
+    "Noto Color Emoji:size=18",
+    "IosevkaZeng Nerd Font:size=18"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
