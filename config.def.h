ec0751f0fce8dc2d0c5bbddaa49dc81a75bd13c3
zengshuai <zengs1994@gmail.com>
2025-11-21 23:22:48 +0800

    reformat and make dmenu_run ignorecase

--- config.def.h
+++ config.def.h
@@ -1,24 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 600;                    /* minimum width when centered */
-static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
+static int         topbar            = 1;    /* -b  option; if 0, dmenu appears at bottom     */
+static int         centered          = 1;    /* -c option; centers dmenu on screen */
+static int         min_width         = 600;  /* minimum width when centered */
+static const float menu_height_ratio = 4.0f; /* This is the ratio used in the original calculation */
+static int         fuzzy             = 1;    /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Noto Sans SC:size=14",
+    "Noto Sans SC:size=14",
     "Noto Color Emoji:size=14",
-	"SauceCodePro Nerd Font:size=14",
-	"TsangerJinKai05:size=14",
+    "SauceCodePro Nerd Font:size=14",
+    "TsangerJinKai05:size=14",
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt                = NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#2e3440" },
-	[SchemeSel] = { "#eeeeee", "#4c566a" },
-	[SchemeOut] = { "#212529", "#81a1c1" },
+    /*     fg         bg       */
+    [SchemeNorm] = {"#bbbbbb", "#2e3440"},
+    [SchemeSel]  = {"#eeeeee", "#4c566a"},
+    [SchemeOut]  = {"#212529", "#81a1c1"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 8;
